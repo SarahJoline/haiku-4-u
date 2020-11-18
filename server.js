d86f3507caa4687b6982991e70a6283fb3f181b6
Sarah Naas <sjnaas@gmail.com>
2020-11-18 13:15:09 -0800

    testing proxy

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 const mongoose = require("mongoose");
 const path = require("path");
 
