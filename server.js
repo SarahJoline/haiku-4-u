1b5778cde52d94b3250ff2e31e9d67642892c86a
Sarah Naas <sarah@wildearth.com>
2024-02-05 19:46:13 +0100

    removed eslint

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 
 dotenv.config();
 
-const MONGO_URI = process.env.MONGODB_URI;
+const MONGO_URI = "mongodb+srv://snaas:Skiwi12@cluster0.cfvok.mongodb.net/";
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
